75ba05c16be53abd1a1dc641d511a3ebfb5a18b9
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-12-03 11:25:09 +0200

    remove index.js and demo test

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require("assert");
-const sayHello = require("../index");
-
-describe("sayHello Function", () => {
-  it("should return 'Hello World!'", () => {
-    assert.strictEqual(sayHello(), "Hello World!");
-  });
-});
